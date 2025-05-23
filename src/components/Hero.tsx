a62c1ef1f31159c1947ed2ec620aecb9d73fb1df
William <williwiser@gmail.com>
2025-05-23 11:07:42 +0200

    hero bug fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import SlideIn from "./SlideIn";
 const Hero = () => {
   return (
     <header
-      className="h-[90dvh] max-h-[700px] overflow-hidden"
+      className="h-[95dvh] max-h-[700px] overflow-hidden"
       style={{
         backgroundColor: "#dbe2e5",
         backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%236797c1' fill-opacity='0.36' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
